283b19f70015b3a71b2af1135625394bb4820224
BearytheGreenBear <liangkyle@gmail.com>
2025-01-20 12:28:25 -0800

    7/2/24

--- USACO/fine-dining.cpp
+++ USACO/fine-dining.cpp
@@ -5,51 +5,34 @@ using namespace std;
 #define se second
 
 const int MOD = 1e9+7;
+const long long LL_INF = (1LL << 60);
+
+const int MAX_N = 5e4;
 
 int N, M, K;
-vector<pair<long long, int>> adjList[50001];
-long long val[500001]; // Value of the K nodes
-long long A[50001]; // Min cost to node N
-long long B[50001]; // Min cost to one of the K nodes
+vector<pair<int, int>> adjList[MAX_N + 5];
+long long dist[MAX_N + 5];
+long long orig[MAX_N + 5];
+bool seen[MAX_N + 5];
 
-void Dijkstra(bool flag) {
+void Dijkstra(int src) {
+    for (int i = 0; i <= N; i++) {
+        seen[i] = false;
+        dist[i] = (i == src ? 0 : LL_INF);
+    }
     priority_queue<pair<long long, int>> pq;
-    vector<bool> seen(N+1);
-    if (flag) {
-        for (int i = 1; i <= N; i++) {
-            A[i] = (1LL<<60);
-        }
-        A[N] = 0LL;
-        pq.push({0LL, N});
-    }
-    else {
-        for (int i = 1; i <= N; i++) {
-            B[i] = (1LL<<60);
-            if (val[i]) {
-                B[i] = A[i]-val[i];
-                pq.push({val[i], i});
-            }
-        }
-    }
+    pq.push({0, src});
     while (!pq.empty()) {
-        int u = pq.top().se;
-        long long curCost = -pq.top().fi;
+        long long curDist = pq.top().fi;
+        int node = pq.top().se;
         pq.pop();
-        if (seen[u]) continue;
-        seen[u] = true;
-        for (auto [c, v]: adjList[u]) {
-            long long newCost = curCost+c;
-            if (flag) {
-                if (newCost < A[v]) {
-                    A[v] = newCost;
-                    pq.push({-newCost, v});
-                }
-            }
-            else {
-                if (newCost < B[v]) {
-                    B[v] = newCost;
-                    pq.push({-newCost, v});
-                }
+        if (seen[node]) continue;
+        seen[node] = true;
+        for (auto [child, weight]: adjList[node]) {
+            long long newDist = -curDist + weight;
+            if (newDist < dist[child]) {
+                dist[child] = newDist;
+                pq.push({-dist[child], child});
             }
         }
     }
@@ -63,23 +46,20 @@ int main() {
         int u, v;
         long long c;
         cin >> u >> v >> c;
-        adjList[u].push_back({c, v});
-        adjList[v].push_back({c, u});
+        adjList[u].push_back({v, c});
+        adjList[v].push_back({u, c});
+    }
+    Dijkstra(N);
+    for (int i = 1; i <= N; i++) {
+        orig[i] = dist[i];
     }
     for (int i = 1; i <= K; i++) {
-        int ind;
-        long long c;
-        cin >> ind >> c;
-        val[ind] = c;
+        int ind, v;
+        cin >> ind >> v;
+        adjList[0].push_back({ind, orig[ind] - v});
     }
-    Dijkstra(true);
-    Dijkstra(false);
+    Dijkstra(0);
     for (int i = 1; i < N; i++) {
-        if (B[i] <= A[i]) {
-            cout << 1 << "\n";
-        }
-        else {
-            cout << 0 << "\n";
-        }
+        cout << (dist[i] <= orig[i]) << "\n";
     }
 }
